9d588263fa705279127848bd6a448291c721a0fe
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-04-22 17:45:36 +0200

    complete ex3

--- src/main/scala/ex3/Solitaire.scala
+++ src/main/scala/ex3/Solitaire.scala
@@ -1,22 +1,70 @@
 package ex3
 
+import javax.swing.text.Position
+
 object Solitaire extends App:
+  import ImplementationHelpers.*
 
-  type Cell = (Int, Int)
-  type Solution = Seq[Cell]
+  type Solution = List[Position]
+  type Solutions = List[Solution]
+  case class Position(x: Int, y: Int)
 
-  def render(solution: Seq[Cell], width: Int, height: Int): String =
-    val reversed = solution.reverse
-    val rows =
-      for
-        y <- 0 until height
-        row = for
-          x <- 0 until width
-          number = reversed.indexOf((x, y)) + 1
-        yield if number > 0 then "%-2d ".format(number) else "X  "
-      yield row.mkString
-    rows.mkString("\n")
+  def placeMarks(width: Int, height: Int): Solutions =
+    val initialPosition = Position(width / 2, height / 2)
+    val startingValue = getStartingValue(width, height)
+    computeSolution(width, height)(initialPosition)(startingValue)
 
-  def placeMarks(width: Int, height: Int): List[Seq[Cell]] = ???
+  def renderSolution(
+      sol: Solution,
+      w: Int,
+      h: Int
+  ): String =
+    render(sol, w, h)
 
-  // println(render(solution = Seq((0, 0), (2, 1)), width = 3, height = 3))
+  private object ImplementationHelpers:
+
+    def computeSolution(w: Int, h: Int)(p: Position)(
+        acc: Int
+    ): Solutions = acc match
+      case 1 => List(List(p))
+      case _ =>
+        for
+          sol <- computeSolution(w, h)(p)(
+            acc - 1
+          )
+          x <- 0 until w
+          y <- 0 until h
+          nextPos = Position(x, y)
+          if isValidMove(sol, sol.last, nextPos)
+        yield sol :+ nextPos
+
+    def isValidMove(
+        sol: Solution,
+        pos: Position,
+        next: Position
+    ): Boolean =
+      def canMove(): Boolean =
+        val dx = (sol.last.x - pos.x).abs
+        val dy = (sol.last.y - pos.y).abs
+        (dx == 2 && dy == 0) || (dx == 0 && dy == 2) || (dx == 1 && dy == 1)
+      canMove() && !sol.contains(next)
+
+    def render(
+        sol: Solution,
+        w: Int,
+        h: Int
+    ): String =
+      val reversed = sol
+      val rows =
+        for
+          y <- 0 until h
+          row = for
+            x <- 0 until w
+            number = reversed.indexOf(Position(x, y)) + 1
+          yield if number > 0 then "%-2d ".format(number) else "X  "
+        yield row.mkString
+      rows.mkString("\n")
+
+    def getStartingValue(width: Int, height: Int): Int =
+      def Middle: Int = (width * height) / 2
+      width * height - Middle
--- src/test/scala/ex3/SolitaireSpec.scala
+++ src/test/scala/ex3/SolitaireSpec.scala
@@ -7,14 +7,14 @@ import ex3.Solitaire.*
 class SolitaireSpec extends AnyFlatSpec with Matchers:
   val solutions = placeMarks(3, 3)
   "render1" should "return a string representation of the board" in {
-    render(
+    renderSolution(
       solutions(0),
       3,
       3
     ) shouldEqual "2  X  5  \nX  1  X  \n3  X  4  "
   }
   "render2" should "return a string representation of the board" in {
-    render(
+    renderSolution(
       solutions(1),
       3,
       3
