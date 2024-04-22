6482519108048d5740da8a6f7b8c72d383663464
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-04-22 10:09:07 +0200

    copy implementation, forgot to make a fork!

--- src/main/scala/ex1/Parser.scala
+++ src/main/scala/ex1/Parser.scala
@@ -1,18 +1,23 @@
 package ex1
 
-/** Consider the Parser example shown in previous lesson. Analogously to NonEmpty, create a mixin NotTwoConsecutive,
-  * which adds the idea that one cannot parse two consecutive elements which are equal. Use it (as a mixin) to build
-  * class NotTwoConsecutiveParser, used in the testing code at the end. Note we also test that the two mixins can work
-  * together!!
+import ex1.Parsers.charParser
+
+/** Consider the Parser example shown in previous lesson. Analogously to
+  * NonEmpty, create a mixin NotTwoConsecutive, which adds the idea that one
+  * cannot parse two consecutive elements which are equal. Use it (as a mixin)
+  * to build class NotTwoConsecutiveParser, used in the testing code at the end.
+  * Note we also test that the two mixins can work together!!
   */
 
 abstract class Parser[T]:
   def parse(t: T): Boolean // is the token accepted?
   def end: Boolean // is it ok to end here
-  def parseAll(seq: Seq[T]): Boolean = (seq forall parse) & end // note &, not &&
+  def parseAll(seq: Seq[T]): Boolean =
+    (seq forall parse) & end // note &, not &&
 
 object Parsers:
-  val todo = ??? // put the extensions here..
+  extension (s: String)
+    def charParser(): Parser[Char] = new BasicParser(s.toSet)
 class BasicParser(chars: Set[Char]) extends Parser[Char]:
   override def parse(t: Char): Boolean = chars.contains(t)
   override def end: Boolean = true
@@ -24,13 +29,23 @@ trait NonEmpty[T] extends Parser[T]:
     super.parse(t) // who is super??
   abstract override def end: Boolean = !empty && super.end
 
-class NonEmptyParser(chars: Set[Char]) extends BasicParser(chars) with NonEmpty[Char]
+class NonEmptyParser(chars: Set[Char])
+    extends BasicParser(chars)
+    with NonEmpty[Char]
 
 trait NotTwoConsecutive[T] extends Parser[T]:
-  val todo = ???
-// ???
+  private[this] var last: T = None.asInstanceOf[T]
+  abstract override def parse(t: T): Boolean =
+    if !(last == null) && last == t then false
+    else
+      last = t
+      super.parse(t)
 
-class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) // with ????
+  abstract override def end: Boolean = super.end
+
+class NotTwoConsecutiveParser(chars: Set[Char])
+    extends BasicParser(chars)
+    with NotTwoConsecutive[Char]
 
 @main def checkParsers(): Unit =
   def parser = new BasicParser(Set('a', 'b', 'c'))
@@ -45,18 +60,21 @@ class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) // wi
   println(parserNE.parseAll("0123".toList)) // false
   println(parserNE.parseAll(List())) // false
 
+  println("parser ntc")
   def parserNTC = new NotTwoConsecutiveParser(Set('X', 'Y', 'Z'))
   println(parserNTC.parseAll("XYZ".toList)) // true
   println(parserNTC.parseAll("XYYZ".toList)) // false
   println(parserNTC.parseAll("".toList)) // true
 
   // note we do not need a class name here, we use the structural type
-  def parserNTCNE = new BasicParser(Set('X', 'Y', 'Z')) with NotTwoConsecutive[Char] with NonEmpty[Char]
+  def parserNTCNE = new BasicParser(Set('X', 'Y', 'Z'))
+    with NotTwoConsecutive[Char]
+    with NonEmpty[Char]
   println(parserNTCNE.parseAll("XYZ".toList)) // true
   println(parserNTCNE.parseAll("XYYZ".toList)) // false
   println(parserNTCNE.parseAll("".toList)) // false
 
-  def sparser: Parser[Char] = ??? // "abc".charParser()
+  def sparser: Parser[Char] = "abc".charParser() // "abc".charParser()
   println(sparser.parseAll("aabc".toList)) // true
   println(sparser.parseAll("aabcdc".toList)) // false
   println(sparser.parseAll("".toList)) // true
